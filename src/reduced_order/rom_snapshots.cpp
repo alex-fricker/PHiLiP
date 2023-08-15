1894756d4cc6a79cbe6d8e96d390688a567f0bb1
Alex Fricker <adfricker@gmail.com>
2023-08-15 10:20:44 -0400

    Backing up

---
+++ src/reduced_order/rom_snapshots.cpp
@@ -0,0 +1,3 @@
+#include "rom_snapshots.hpp"
+#include "pod_basis_online.h"
+#include "eigen/Eigen/"
---
+++ src/reduced_order/rom_snapshots.hpp
@@ -0,0 +1,22 @@
+#ifndef _ROM_SNAPSHOTS__
+#define _ROM_SNAPSHOTS__
+
+namespace PHiLiP {
+namespace ProperOrthogonalDecomposition{
+
+template <int dim>
+class RomSnapshots
+{
+public:
+    /// Constructor
+    RomSnapshots(const int n_snapshots);
+
+    /// Destructor
+    ~RomSnapshots() {};
+
+
+}
+
+
+}  // POD namespace
+}  // PHiLiP namespace
